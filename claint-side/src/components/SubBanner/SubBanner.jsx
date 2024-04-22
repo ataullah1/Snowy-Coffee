f05b910c7d80fe919ccb180b23b155ff00ee4347
Md Ataullah <ataullahm100@gmail.com>
2024-04-22 08:59:58 +0600

    Sub Banner section added

---
+++ claint-side/src/components/SubBanner/SubBanner.jsx
@@ -0,0 +1,48 @@
+import img1 from '../../assets/images/icons/1.png';
+import img2 from '../../assets/images/icons/2.png';
+import img3 from '../../assets/images/icons/3.png';
+import img4 from '../../assets/images/icons/4.png';
+const SubBanner = () => {
+  return (
+    <div className="w-full h-[300px] bg-stone-200 md:w-11/12 lg:w-10/12 mx-auto">
+      <div>
+        <img className="h-14 md:h-[70] w-14 md:w-[70]" src={img1} alt="" />
+        <h2 className="text-stone-800 text-[35px] font-normal">
+          Awesome Aroma
+        </h2>
+        <p className="max-w-[301px] text-stone-900 text-base font-normal">
+          You will definitely be a fan of the design & aroma of your coffee
+        </p>
+      </div>
+      <div>
+        <img className="h-14 md:h-[70] w-14 md:w-[70]" src={img1} alt="" />
+        <h2 className="text-stone-800 text-[35px] font-normal">
+          Awesome Aroma
+        </h2>
+        <p className="max-w-[301px] text-stone-900 text-base font-normal">
+          You will definitely be a fan of the design & aroma of your coffee
+        </p>
+      </div>
+      <div>
+        <img className="h-14 md:h-[70] w-14 md:w-[70]" src={img1} alt="" />
+        <h2 className="text-stone-800 text-[35px] font-normal">
+          Awesome Aroma
+        </h2>
+        <p className="max-w-[301px] text-stone-900 text-base font-normal">
+          You will definitely be a fan of the design & aroma of your coffee
+        </p>
+      </div>
+      <div>
+        <img className="h-14 md:h-[70] w-14 md:w-[70]" src={img1} alt="" />
+        <h2 className="text-stone-800 text-[35px] font-normal">
+          Awesome Aroma
+        </h2>
+        <p className="max-w-[301px] text-stone-900 text-base font-normal">
+          You will definitely be a fan of the design & aroma of your coffee
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SubBanner;
