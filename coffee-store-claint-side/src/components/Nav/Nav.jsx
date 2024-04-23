59a64b9979f72355ba705b3c664bae63fda44d56
Md Ataullah <ataullahm100@gmail.com>
2024-04-23 19:24:30 +0600

    navber responsived

--- coffee-store-claint-side/src/components/Nav/Nav.jsx
+++ coffee-store-claint-side/src/components/Nav/Nav.jsx
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo1 from '../../assets/images/more/logo1.png';
 import { RiMenuAddLine } from 'react-icons/ri';
+import { useState } from 'react';
 const Nav = () => {
+  const [menu, setMenu] = useState(false);
   return (
     <div className="bg-[#372727] ">
       <div className="py-2 flex gap-4 items-center justify-between w-11/12 lg:w-10/12 mx-auto">
@@ -12,26 +14,53 @@ const Nav = () => {
           </h1>
         </div>
         <div className="relative">
-          <div className="text-white text-3xl cursor-pointer sm:hidden">
+          <div
+            className="text-white text-3xl cursor-pointer sm:hidden"
+            onClick={() => setMenu(!menu)}
+          >
             <RiMenuAddLine />
           </div>
-          <ul className="absolute right-0 top-10 border-2 sm:border-none border-white py-3 sm:py-0 min-w-32 rounded-md z-20 bg-primaryColor sm:bg-transparent sm:static flex flex-col sm:flex-row items-center gap-2 sm:gap-5 lg:gap-8 text-white">
-            <li className="border-b border-transparent hover:border-[#E3B577]">
-              <Link>Home</Link>
-            </li>
-            <li className="border-b border-transparent hover:border-[#E3B577]">
-              <Link>Add Coffee</Link>
-            </li>
-            <li className="border-b border-transparent hover:border-[#E3B577]">
-              <Link>About</Link>
-            </li>
-            <li className="border-2 mt-1 sm:mt-0 border-white bg-[#E3B577] px-5 md:px-7 py-1 md:py-2 rounded-md font-bold">
-              <Link>Login</Link>
-            </li>
+          {menu && (
+            <ul className="absolute right-0 top-10 border-2  border-white py-3 min-w-32 rounded-md z-20 bg-primaryColor flex flex-col items-center gap-2 text-white sm:hidden">
+              <li className="border-b border-transparent hover:border-[#E3B577]">
+                <NavLink to={'/'}>Home</NavLink>
+              </li>
+              <li className="border-b border-transparent hover:border-[#E3B577]">
+                <NavLink to={'/add-coffee'}>Add Coffee</NavLink>
+              </li>
+              <li className="border-b border-transparent hover:border-[#E3B577]">
+                <NavLink to={'/about'}>About</NavLink>
+              </li>
+              <NavLink to={'/login'}>
+                <li className="mt-1 sm:mt-0 bg-[#E3B577] px-7 py-2 md:py-2 rounded-sm font-bold text-sm cursor-pointer">
+                  Login
+                </li>
+              </NavLink>
 
-            {/* <div>
+              {/* <div>
               <div className="h-10 w-10 md:h-14 md:w-14 cursor-pointer border-2 border-white rounded-full"></div>
             </div> */}
+            </ul>
+          )}
+          <ul className="hidden sm:flex flex-row items-center gap-2 sm:gap-5 lg:gap-8 text-white">
+            <li className="border-b border-transparent hover:border-[#E3B577]">
+              <NavLink to={'/'}>Home</NavLink>
+            </li>
+            <li className="border-b border-transparent hover:border-[#E3B577]">
+              <NavLink to={'/add-coffee'}>Add Coffee</NavLink>
+            </li>
+            <li className="border-b border-transparent hover:border-[#E3B577]">
+              <NavLink to={'/about'}>About</NavLink>
+            </li>
+            <NavLink to={'/login'}>
+              <li className="border-2 mt-1 sm:mt-0 border-transparent hover:border-[#E3B577] bg-[#E3B577] hover:bg-transparent px-5 md:px-7 py-1 md:py-2 rounded-md font-bold active:scale-90 duration-200">
+                Login
+              </li>
+            </NavLink>
+
+            {/* <div>
+          <div className="h-10 w-10 md:h-14 md:w-14 cursor-pointer border-2 border-white rounded-full"></div>
+        </div> */}
           </ul>
         </div>
       </div>
