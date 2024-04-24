6c8cdee746c05705ec10913097b36e6a89157522
Md Ataullah <ataullahm100@gmail.com>
2024-04-24 11:48:06 +0600

    Error solved

--- coffee-store-claint-side/src/PrivetRoute/PrivetRoute.jsx
+++ coffee-store-claint-side/src/PrivetRoute/PrivetRoute.jsx
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
-import { ContextAuth } from '../../provider/Provider';
 import { Navigate, useLocation } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
+import { useContext } from 'react';
+import { ContextAuth } from '../provider/Provider';
 
 const PrivetRoute = ({ children }) => {
   const { userDta } = useContext(ContextAuth);
